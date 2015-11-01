41616dc6fe1ab03e0eefc8d2a65232bf61559364
Derek316x <dereknetto@gmail.com>
2015-11-01 14:19:24 -0500

    before pjo

--- DiversityPDFReader/DiversityPDFReader/MFDocumentManagerViewController.m
+++ DiversityPDFReader/DiversityPDFReader/MFDocumentManagerViewController.m
@@ -11,6 +11,8 @@
 
 @interface MFDocumentManagerViewController () <SlidingViewDelegate>
 
+@property (nonatomic) BOOL isRecording;
+
 @end
 
 const unsigned char SpeechKitApplicationKey[] = {0xb1, 0x0e, 0x79, 0x35, 0x3d, 0xbf, 0xe0, 0xfc, 0xf4, 0xc1, 0xad, 0x14, 0xd3, 0xea, 0x78, 0x50, 0x44, 0x5d, 0xbc, 0xd5, 0x40, 0x86, 0xdb, 0xef, 0x85, 0x5c, 0xe9, 0x1f, 0xb1, 0xba, 0xde, 0x62, 0xc8, 0x57, 0x98, 0x06, 0x16, 0x78, 0x02, 0x0a, 0x3a, 0xb0, 0x1f, 0x0d, 0x9a, 0x97, 0x5d, 0xde, 0xf5, 0x19, 0xb4, 0x35, 0xa0, 0x8f, 0x25, 0x25, 0xeb, 0x0e, 0x44, 0x9c, 0xc8, 0x22, 0xfe, 0x90};
@@ -28,6 +30,7 @@ const unsigned char SpeechKitApplicationKey[] = {0xb1, 0x0e, 0x79, 0x35, 0x3d, 0
                     useSSL:NO
                   delegate:nil];
     
+    self.isRecording = NO;
 }
 
 -(void)recognizerDidBeginRecording:(SKRecognizer *)recognizer{
@@ -41,8 +44,23 @@ const unsigned char SpeechKitApplicationKey[] = {0xb1, 0x0e, 0x79, 0x35, 0x3d, 0
     NSString* search = [results firstResult];
     self.recordedString = search;
     
-    
     //do testy shit
+    NSString *correctText = @"A dictionary is a collection of data consisting of key object pairs. Just as you would look up the definition of a word in a dictionary, you obtain the value object from an Objective-C dictionary by its key. The keys in a dictionary must be unique, and they can be of any object type, although they are typically strings. The value associated with the key can also be of any object type, but it cannot be nil.";
+    NSString *noPunctuationCorrectText = [[[correctText stringByReplacingOccurrencesOfString:@"." withString:@""] stringByReplacingOccurrencesOfString:@"," withString:@""] lowercaseString];
+    NSString *noPunctuationSpeechText = [[[self.recordedString stringByReplacingOccurrencesOfString:@"." withString:@""] stringByReplacingOccurrencesOfString:@"," withString:@""] lowercaseString];
+
+   float levenshteinDistance = [self compareString:noPunctuationCorrectText withString:noPunctuationSpeechText];
+    float charactersInCorrectText = (float)[noPunctuationCorrectText length];
+    
+    CGFloat percentage = (1.0 - (levenshteinDistance/charactersInCorrectText)) * 100;
+    
+    if (percentage > 15) {
+        percentage+=25;
+        if (percentage>100) {
+            percentage = 100;
+        }
+    }
+
 }
 
 
@@ -86,15 +104,105 @@ const unsigned char SpeechKitApplicationKey[] = {0xb1, 0x0e, 0x79, 0x35, 0x3d, 0
 
 -(void)buttonPressed{
     NSLog(@"it works!");
-    NSString *text = [self.documentManager wholeTextForPage:381];
     
-    [self resignFirstResponder];
-    self.recognizer = [[SKRecognizer alloc] initWithType:SKSearchRecognizerType detection:SKLongEndOfSpeechDetection language:@"en_US" delegate:self];
-    
-    NSString *correctText = @"A dictionary is a collection of data consisting of key-object pairs. Just as you would look up the definition of a word in a dictionary, you obtain the value (object) from an Objective-C dictionary by its key. The keys in a dictionary must be unique, and they can be of any object type, although they are typically strings. The value associated with the key can also be of any object type, but it cannot be nil.";
+    self.isRecording = !self.isRecording;
+  
+    if (self.isRecording == YES) {
+        
+        [self resignFirstResponder];
+        self.recognizer = [[SKRecognizer alloc] initWithType:SKSearchRecognizerType detection:SKNoEndOfSpeechDetection language:@"en_US" delegate:self];
+    }
+    else{
+        [self.recognizer stopRecording];
+    }
     
 }
 
+#pragma mark - Levenshtein Distance Algorithm
+
+-(float)compareString:(NSString *)originalString withString:(NSString *)comparisonString
+{
+    // Normalize strings
+    [originalString stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+    [comparisonString stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+    
+    originalString = [originalString lowercaseString];
+    comparisonString = [comparisonString lowercaseString];
+    
+    // Step 1 (Steps follow description at http://www.merriampark.com/ld.htm)
+    NSInteger k, i, j, cost, * d, distance;
+    
+    NSInteger n = [originalString length];
+    NSInteger m = [comparisonString length];
+    
+    if( n++ != 0 && m++ != 0 ) {
+        
+        d = malloc( sizeof(NSInteger) * m * n );
+        
+        // Step 2
+        for( k = 0; k < n; k++)
+            d[k] = k;
+        
+        for( k = 0; k < m; k++)
+            d[ k * n ] = k;
+        
+        // Step 3 and 4
+        for( i = 1; i < n; i++ )
+            for( j = 1; j < m; j++ ) {
+                
+                // Step 5
+                if( [originalString characterAtIndex: i-1] ==
+                   [comparisonString characterAtIndex: j-1] )
+                    cost = 0;
+                else
+                    cost = 1;
+                
+                // Step 6
+                d[ j * n + i ] = [self smallestOf: d [ (j - 1) * n + i ] + 1
+                                            andOf: d[ j * n + i - 1 ] +  1
+                                            andOf: d[ (j - 1) * n + i - 1 ] + cost ];
+                
+                // This conditional adds Damerau transposition to Levenshtein distance
+                if( i>1 && j>1 && [originalString characterAtIndex: i-1] ==
+                   [comparisonString characterAtIndex: j-2] &&
+                   [originalString characterAtIndex: i-2] ==
+                   [comparisonString characterAtIndex: j-1] )
+                {
+                    d[ j * n + i] = [self smallestOf: d[ j * n + i ]
+                                               andOf: d[ (j - 2) * n + i - 2 ] + cost ];
+                }
+            }
+        
+        distance = d[ n * m - 1 ];
+        
+        free( d );
+        
+        return distance;
+    }
+    return 0.0;
+}
+
+// Return the minimum of a, b and c - used by compareString:withString:
+-(NSInteger)smallestOf:(NSInteger)a andOf:(NSInteger)b andOf:(NSInteger)c
+{
+    NSInteger min = a;
+    if ( b < min )
+        min = b;
+    
+    if( c < min )
+        min = c;
+    
+    return min;
+}
+
+-(NSInteger)smallestOf:(NSInteger)a andOf:(NSInteger)b
+{
+    NSInteger min=a;
+    if (b < min)
+        min=b;
+    
+    return min;
+}
 
 
 @end
--- DiversityPDFReader/DiversityPDFReader/SlidingView.m
+++ DiversityPDFReader/DiversityPDFReader/SlidingView.m
@@ -14,15 +14,17 @@
     self = [super initWithFrame:frame];
     if(self){
         
+        CGFloat buttonSize = 20;
+        
         UIPanGestureRecognizer *panRecognizer = [[UIPanGestureRecognizer alloc]initWithTarget:self action:@selector(detectPan:)];
         self.gestureRecognizers = @[panRecognizer];
         
-        UIColor *redColor = [UIColor colorWithRed:206/255.0 green:17/255.0 blue:38/255.0 alpha:1.0];
+        UIColor *redColor = [UIColor colorWithRed:206/255.0 green:17/255.0 blue:38/255.0 alpha:0.9];
         UIColor *orangeColor = [UIColor colorWithRed:255/255.0 green:123/255.0 blue:0/255.0 alpha:0.3];
         
         //add button
         UIButton *speechTestButton = [UIButton buttonWithType: UIButtonTypeRoundedRect];
-        speechTestButton.frame = CGRectMake(0, 0, self.bounds.size.width, 30);
+        speechTestButton.frame = CGRectMake(0, 0, self.bounds.size.width, buttonSize);
         [speechTestButton setTitle:@"" forState:UIControlStateNormal];
         [speechTestButton addTarget:self action:@selector(speechTestButtonPressed) forControlEvents:UIControlEventTouchUpInside];
         [self addSubview:speechTestButton];
@@ -35,7 +37,7 @@
         //[self addSubview:orangeView];
         
         //add red box on top of orange transparent box
-        UIView *redView = [[UIView alloc] initWithFrame:CGRectMake(0, 30, self.bounds.size.width, self.bounds.size.height)];
+        UIView *redView = [[UIView alloc] initWithFrame:CGRectMake(0, buttonSize, self.bounds.size.width, self.bounds.size.height)];
         redView.backgroundColor = redColor;
         [self addSubview:redView];
         
@@ -45,7 +47,6 @@
 
 -(void)speechTestButtonPressed{
     [self.delegate buttonPressed];
-    NSLog(@"BUTTON PRESSED");
 }
 
 -(void) detectPan: (UIPanGestureRecognizer *) uiPanGestureRecognizer{
